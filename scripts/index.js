6e15ded1f921e60da77364c3bee461511074896d
Miguel <miguelkunzer567@gmail.com>
2025-02-06 22:24:05 -0300

    Add basic input UI

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
+function BoardHandler() {
+    this.WinningCombinations = [
+        //Horizontal
+        ['top-left', 'top-mid', 'top-right'],
+        ['mid-left', 'mid-mid', 'mid-right'],
+        ['bottom-left', 'bottom-mid', 'bottom-right'],
 
-function BoardHandler(){
-    this.WinningCombinations =[
-    //Horizontal
-    ['top-left','top-mid','top-right'],
-    ['mid-left','mid-mid','mid-right'],
-    ['bottom-left','bottom-mid','bottom-right'],
+        //Vertical
+        ['top-left', 'mid-left', 'bottom-left'],
+        ['top-mid', 'mid-mid', 'bottom-mid'],
+        ['top-right', 'mid-right', 'bottom-right'],
 
-    //Vertical
-    ['top-left','mid-left','bottom-left'],
-    ['top-mid','mid-mid','bottom-mid'],
-    ['top-right','mid-right','bottom-right'],
-    
-    //Diagonal
-    ['top-left','mid-mid','bottom-right'],
-    ['top-right','mid-mid','bottom-left']
-];
+        //Diagonal
+        ['top-left', 'mid-mid', 'bottom-right'],
+        ['top-right', 'mid-mid', 'bottom-left']
+    ];
     this.selections = [];
 
     const Box = document.getElementById('game');
+
     const board = [
-    'top-left','top-mid','top-right',
-    'mid-left','mid-mid','mid-right',
-    'bottom-left','bottom-mid','bottom-right'
+        'top-left', 'top-mid', 'top-right',
+        'mid-left', 'mid-mid', 'mid-right',
+        'bottom-left', 'bottom-mid', 'bottom-right'
     ];
 
-    const configStyleElement = function(item){
+    const configStyleElement = function (item) {
         const selectionElement = document.createElement('div');
         selectionElement.id = item;
         selectionElement.className = 'selection_section';
@@ -32,83 +32,104 @@ function BoardHandler(){
         return selectionElement;
     };
 
-    board.forEach(item=>{
+    board.forEach(item => {
         const element = configStyleElement(item);
         this.selections.push(element);
     });
-};
+}
 
-function Player(name,symbol){
+function Player(name, symbol) {
     this.name = name;
     this.symbol = symbol;
     this.ButtonsSelected = [];
-};
+}
 
-function GameHandler(PlayerOne,PlayerTwo){
+function GameHandler(PlayerOne, PlayerTwo) {
     let CurrentPlayer = PlayerOne;
+    let Board; 
 
-    function deactivateGame(){
-        
-    }
+    function updateStatus() {
 
-    function checkStatus(WinningCombinations){
-        const numSequenceToWin = 3;
-        WinningCombinations.forEach(item=>{
+    };
+
+    function updateStatusNames() {
+        document.getElementById('player_one').textContent = PlayerOne.name;
+        document.getElementById('player_two').textContent = PlayerTwo.name;
+    };
+
+    function handleClick() {
+        toMark(this);
+        setTimeout(() => {
+            checkVitory(Board.WinningCombinations); 
+            CurrentPlayer = CurrentPlayer === PlayerOne ? PlayerTwo : PlayerOne;
+        }, 0);
+    };
+
+    function activateGame() {
+        updateStatusNames();
+
+        Board = new BoardHandler();  
+        const BoardSelections = Board.selections;
+
+        BoardSelections.forEach((selection) => {
+            selection.addEventListener('click', handleClick, { once: true });
+        });
+    };
+
+    function deactivateGame() {
+        const elements = document.getElementsByClassName('selection_section');
+        Object.values(elements).forEach((value) => {
+            value.classList.add('no-hover');
+
+            value.removeEventListener('click', handleClick);
+        });
+    };
+
+    function checkVitory(WinningCombinations) {  
+        WinningCombinations.forEach(item => {
+
             let numSequenceOfPlayer = 0;
-
-            Object.values(CurrentPlayer.ButtonsSelected).forEach(value=>{
+            Object.values(CurrentPlayer.ButtonsSelected).forEach(value => {
                 const result = item.includes(value);
-                if (result === true){
-                    numSequenceOfPlayer += 1
+                if (result === true) {
+                    numSequenceOfPlayer += 1;
                 };
-                if (numSequenceOfPlayer === 3){
+                if (numSequenceOfPlayer === 3) {
                     winner(CurrentPlayer);
+                    deactivateGame();
                 };
             });
         });
     };
 
     function toMark(selection) {
-        const marker = "marker_"+CurrentPlayer.symbol
-        selection.classList.add(marker)
-        CurrentPlayer.ButtonsSelected.push(selection.id)
-        console.log(CurrentPlayer.name+"="+CurrentPlayer.ButtonsSelected)
+        const marker = "marker_" + CurrentPlayer.symbol;
+        selection.classList.add(marker);
+        CurrentPlayer.ButtonsSelected.push(selection.id);
+        console.log(CurrentPlayer.name + "=" + CurrentPlayer.ButtonsSelected);
         selection.classList.add('no-hover');
     };
 
-    function winner(plr){
-        console.log(plr.name,"venceu")
-    }
-
-    this.Start = function(){
-        const Board = new BoardHandler();
-        const BoardSelections = Board.selections;
-        const WinningCombinations = Board.WinningCombinations;
-
-        BoardSelections.forEach(selection=>{
-            selection.addEventListener('click',()=>{
-                toMark(selection);
-                checkStatus(WinningCombinations);
-                CurrentPlayer = CurrentPlayer === PlayerOne ? PlayerTwo : PlayerOne;
-
-            ;},
-            {once:true});
-        });
+    function winner(plr) {
+        alert(plr.name + " venceu");
     };
 
-    this.End = function(){
+    this.Start = function () {
+        activateGame();
     };
 
-    this.Restart = function(){
-
+    this.End = function () {
+        deactivateGame();
     };
-    
-    
+
+    this.Restart = function () {
+        deactivateGame();
+        setTimeout(() => { activateGame() }, 1); 
+    };
 };
 
-const PO = new Player("Percy","X");
-const PT = new Player("Frank","O");
-const Game = new GameHandler(PO,PT);
+function takePlayerNames(){
 
-Game.Start();
+}
+
 
